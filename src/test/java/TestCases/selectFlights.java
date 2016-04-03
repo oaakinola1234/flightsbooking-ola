acb3da623b3c97808c16fe97f37de6d56833833a
Ola Akinola <oa_akinola@live.co.uk>
2016-04-03 21:01:20 +0100

    Sunday grinding

--- src/test/java/TestCases/selectFlights.java
+++ src/test/java/TestCases/selectFlights.java
@@ -7,11 +7,7 @@ import utilities.Constants;
 import utilities.ExcelUtility;
 import utilities.Screenshot;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.AfterMethod;
@@ -19,129 +15,59 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class selectFlights {
-	
+
 	private WebDriver driver;
-	private Screenshot screenshot;
 	private String baseURL;
 	ExpediaHomePageFactory expediaHomePage;
 	static Logger log = Logger.getLogger(selectFlights.class);
-	
+
 	@BeforeClass
-	public void beforeClass() throws Exception{
+	public void beforeClass() throws Exception {
 		driver = new FirefoxDriver();
 		baseURL = "http://www.expeida.co.uk";
 		expediaHomePage = new ExpediaHomePageFactory(driver);
-		
+
 		driver.manage().window().maximize();
 		PropertyConfigurator.configure("log4j.properties");
 		driver.get(Constants.URL);
 		// Tell the code about the location of Excel file
 		ExcelUtility.setExcelFile(Constants.File_Path + Constants.File_Name, "flightsSearch");
 	}
-	
+
 	@BeforeMethod
-	public void beforeMethod(){
+	public void beforeMethod() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-	
+
 	@AfterMethod
-	public void afterMethod() throws InterruptedException{
+	public void afterMethod() throws InterruptedException {
 		driver.get(Constants.URL);
-		
 	}
-	
+
 	@DataProvider(name = "searchFlightsData")
 	public Object[][] dataProvider() {
 		Object[][] testData = ExcelUtility.getTestData("searchFlightsDataExcel");
 		return testData;
 	}
-	
-	@Test(dataProvider="searchFlightsData")
-	public void simpleFlightTest(String originCity, String destinationCity, String departureDate, String returnDate, String numOfAdults) throws Exception{
-		expediaHomePage.clickFlightsTab();
-		expediaHomePage.setOriginCity(originCity);
-		expediaHomePage.setDestinationCity(destinationCity);
-		expediaHomePage.setDepartureDate(departureDate);
-		expediaHomePage.setReturnnDate(returnDate);
-		expediaHomePage.selectAdultsOnFlight(numOfAdults);
-		expediaHomePage.clickSearchButton();
-		boolean isPresent = driver.findElements(By.xpath("//*[@id='acol-interstitial']/h3")).size() > 0;
-		if(isPresent){
-		System.out.println("Finding the cities");
-	}else{
-		System.out.println("Something went wrong");
-		screenshot = new Screenshot(driver, "C:\\Users\\OlaA\\workspace\\flightsbooking-ola\\");
-	}		
-		/*{
-		System.out.println("Something went wrong");
-		String fileName = getRandomString(10) + ".png";
-		String directory = "C:\\Users\\OlaA\\workspace\\flightsbooking-ola\\";
-		
-		File sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(sourceFile, new File(directory + fileName));
-	}*/
+
+	@Test(dataProvider = "searchFlightsData")
+	public void simpleFlightTest(String originCity, String destinationCity, String departureDate, String returnDate,
+			String numOfAdults) throws Exception {
+		expediaHomePage.simpleFlight(originCity, destinationCity, departureDate, returnDate, numOfAdults);
 	}
-	
+
 	@Test
-	public void firstTestMethod(){
+	public void firstTestMethod() {
 		System.out.println("First test method");
 	}
-	
+
 	@Test
-	public void secondTestMethod(){
+	public void secondTestMethod() {
 		System.out.println("Second test method");
 	}
 
-	public static String getRandomString(int length) {
-		StringBuilder sb = new StringBuilder();
-		String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-		for (int i = 0; i < length; i++) {
-			int index = (int) (Math.random() * characters.length());
-			sb.append(characters.charAt(index));
-		}
-		return sb.toString();
-	}    
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
--- src/test/java/page/classes/ExpediaHomePageFactory.java
+++ src/test/java/page/classes/ExpediaHomePageFactory.java
@@ -1,106 +1,130 @@
 package page.classes;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import utilities.Screenshot;
 
 public class ExpediaHomePageFactory {
+	
 	WebDriver driver;
 	static Logger log = Logger.getLogger(ExpediaHomePageFactory.class);
-	
-	@FindBy(id="header-history")
+	private Screenshot screenshot;
+
+	@FindBy(id = "header-history")
 	WebElement headerHistoryLink;
-	
-	@FindBy(id="tab-flight-tab")
+
+	@FindBy(id = "tab-flight-tab")
 	WebElement flightsTabLink;
-	
-	@FindBy(id="flight-origin")
+
+	@FindBy(id = "flight-origin")
 	WebElement originCityTextBox;
-	
-	@FindBy(id="flight-destination")
+
+	@FindBy(id = "flight-destination")
 	WebElement destinationCityTextBox;
-	
-	@FindBy(id="flight-departing")
+
+	@FindBy(id = "flight-departing")
 	WebElement departureDateTextBoxDatePicker;
-	
-	@FindBy(id="flight-returning")
+
+	@FindBy(id = "flight-returning")
 	WebElement returnDateTextBoxDatePicker;
-	
-	@FindBy(id="flight-adults") //drop down 
+
+	@FindBy(id = "flight-adults") // drop down
 	WebElement adultsOnFlightDropDown;
-	
-	@FindBy(id="flight-children") //drop down 
+
+	@FindBy(id = "flight-children") // drop down
 	WebElement childrenOnFlightDropDown;
-	
-	@FindBy(id="advanced-options")
+
+	@FindBy(id = "advanced-options")
 	WebElement advancedOptionLink;
-	
-	@FindBy(id="advanced-flight-nonstop")
+
+	@FindBy(id = "advanced-flight-nonstop")
 	WebElement directFlightCheckBox;
-	
-	@FindBy(id="flight-add-hotel-checkbox")
+
+	@FindBy(id = "flight-add-hotel-checkbox")
 	WebElement addHotelCheckBox;
-	
-	@FindBy(id="flight-add-car-checkbox")
+
+	@FindBy(id = "flight-add-car-checkbox")
 	WebElement addCarCheckBox;
-	
-	@FindBy(id="advanced-flight-refundable")
+
+	@FindBy(id = "advanced-flight-refundable")
 	WebElement refundableFlightCheckBox;
-	
-	@FindBy(id="flight-advanced-preferred-airline") //drop down 
+
+	@FindBy(id = "flight-advanced-preferred-airline") // drop down
 	WebElement preferredAirlineDropDown;
-	
-	@FindBy(id="flight-advanced-preferred-class") //drop down 
+
+	@FindBy(id = "flight-advanced-preferred-class") // drop down
 	WebElement preferredClassDropDown;
-	
-	@FindBy(id="search-button")
+
+	@FindBy(id = "search-button")
 	WebElement searchButton;
-	
-	
-	public ExpediaHomePageFactory(WebDriver driver){
+
+	public ExpediaHomePageFactory(WebDriver driver) {
 		this.driver = driver;
-		PageFactory.initElements(driver, this);		
-	}
-	
-	public void clickFlightsTab(){
-		headerHistoryLink.click();
-		flightsTabLink.click();
-		log.info("headerHistoryLink and flightsTabLink clicked");
-	}
-	
-	public void setOriginCity(String originCityValue){
-		originCityTextBox.sendKeys(originCityValue);
-		log.info("originCityTextBox value set");
-	}
-	
-	public void setDestinationCity(String destinationCity){
-		destinationCityTextBox.sendKeys(destinationCity);
-		log.info("destinationCityTextBox value set");
-	}
-	
-	public void setDepartureDate(String departureDate){
-		departureDateTextBoxDatePicker.sendKeys(departureDate);
-		log.info("departureDateTextBoxDatePicker value set");
+		PageFactory.initElements(driver, this);
 	}
 
-	public void setReturnnDate(String returnDate){
-		returnDateTextBoxDatePicker.sendKeys(returnDate);
-		log.info("returnDateTextBoxDatePicker value set");
+	public void clickFlightsTab() {
+		headerHistoryLink.click();
+		flightsTabLink.click();
+		//log.info("headerHistoryLink and flightsTabLink clicked");
 	}
-	
-	public void selectAdultsOnFlight(String numOfAdults){
+
+	public void setOriginCity(String originCityValue) {
+		originCityTextBox.sendKeys(originCityValue);
+		//log.info("originCityTextBox value set");
+	}
+
+	public void setDestinationCity(String destinationCity) {
+		destinationCityTextBox.sendKeys(destinationCity);
+		//log.info("destinationCityTextBox value set");
+	}
+
+	public void setDepartureDate(String departureDate) {
+		departureDateTextBoxDatePicker.sendKeys(departureDate);
+		//log.info("departureDateTextBoxDatePicker value set");
+	}
+
+	public void setReturnnDate(String returnDate) {
+		returnDateTextBoxDatePicker.sendKeys(returnDate);
+		//log.info("returnDateTextBoxDatePicker value set");
+	}
+
+	public void selectAdultsOnFlight(String numOfAdults) {
 		Select selAdultsOnFlight = new Select(adultsOnFlightDropDown);
 		selAdultsOnFlight.selectByValue(numOfAdults);
-		log.info("adultsOnFlightDropDown value selected");
+		//log.info("adultsOnFlightDropDown value selected");
 	}
-	
-	public void clickSearchButton(){
+
+	public void clickSearchButton() {
 		searchButton.click();
-		log.info("searchButton clicked");
+		//log.info("searchButton clicked");
 	}
+
+	public void simpleFlight(String originCity, String destinationCity, String departureDate, String returnDate,
+			String numOfAdults) throws Exception {
+		clickFlightsTab();
+		setOriginCity(originCity);
+		setDestinationCity(destinationCity);
+		setDepartureDate(departureDate);
+		setReturnnDate(returnDate);
+		selectAdultsOnFlight(numOfAdults);
+		clickSearchButton();
+		boolean isPresent = driver.findElements(By.xpath("//*[@id='acol-interstitial']/h3")).size() > 0;
+		if (!isPresent) {
+			log.info("Something went wrong in the simpleFlightTest");
+			System.out.println("Something went wrong");
+			screenshot = new Screenshot(driver, "C:\\Users\\OlaA\\workspace\\flightsbooking-ola\\");
+			Assert.assertFalse(true);
+		} 
+	}
+
 	
 	
 	
@@ -138,4 +162,28 @@ public class ExpediaHomePageFactory {
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
